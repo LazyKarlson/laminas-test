d44cb02ee3d019333e78d5ceca558f8ffd74cbb6
Yury <yurha@protonmail.com>
2023-02-26 18:05:03 +0300

    postgres setup

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -13,5 +13,16 @@
  */
 
 return [
-    // ...
+    'db' => [
+        'driver' => getenv('DB_DRIVER'),
+        'hostname' => getenv('DB_HOSTNAME'),
+        'port' => getenv('DB_PORT'),
+        'database' => getenv('DB_DATABASE'),
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+        'charset' => getenv('DB_CHARSET'),
+        'options' => [
+            'buffer_results' => true,
+        ],
+    ]
 ];
