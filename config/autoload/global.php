aa93d002aa0a82b369dedd15507a53ecc46677eb
Yury <yurha@protonmail.com>
2023-02-26 18:34:01 +0300

    pgsql

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,11 +12,11 @@
  * file.
  */
 
-return [    
+return [
     'db' => [
         'driver' => 'Pdo',
-        'dsn'    => 'pgsql:host=getenv('DB_HOST');port=getenv('DB_PORT');dbname=getenv('DB_DATABASE');',
         'user' => getenv('DB_USER'),
         'password' => getenv('DB_PASS'),
+        'dsn' => 'pgsql:host=pgsql;port=5432;dbname=laminas;',
     ]
 ];
